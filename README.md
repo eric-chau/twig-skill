2fe6092841e08ebe9b6a80878b5baec4b83049c3
Eric Chau <eriic.chau@gmail.com>
2015-09-19 15:59:05 +0000

    added ContainerProvider and updated README.md with some explanations

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Twig skill
 
 This skill is an integration of `Twig` into Jarvis micro-framework.
+
+## Configuration options
+
+To pass options to Twig you must provide a `twig` settings key like this:
+
+```php
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$jarvis = new Jarvis\Jarvis([
+    'container_provider' => [
+        'Jarvis\Skill\Twig\ContainerProvider',
+    ],
+    'twig' => [
+        'templates_paths' => '/path/to/templates',
+    ],
+]);
+```
+
+Note that `templates_paths` is a required parameter. This skill changes default values for some options:
+
+- `debug`: if not provided, this value take the value of Jarvis `debug` parameter.
+- `auto_reload`: this is setted to `true` as default value in this skill.
+- `strict_variables`: this option is also setted to `true` by default.
+
+You can see complete options list on [Twig documentation](http://twig.sensiolabs.org/doc/api.html#environment-options).
---
+++ src/ContainerProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Jarvis\Skill\Twig;
+
+use Jarvis\Jarvis;
+use Jarvis\Skill\DependencyInjection\ContainerProviderInterface;
+
+/**
+ * @author Eric Chau <eriic.chau@gmail.com>
+ */
+class ContainerProvider implements ContainerProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function hydrate(Jarvis $container)
+    {
+        $container['twig'] = function ($container) {
+            $config = array_merge(
+                [
+                    'auto_reload'      => true,
+                    'debug'            => $container->debug,
+                    'strict_variables' => true,
+                ],
+                $container->settings->get('twig', [])
+            );
+
+            if (!isset($config['templates_paths'])) {
+                throw new \LogicException('Parameter `templates_paths` is missing to configure Twig.');
+            }
+
+            return new \Twig_Environment(new \Twig_Loader_Filesystem($config['templates_paths']), $config);
+        };
+    }
+}
